275ebb6983e14c4324757dd1165235d9d259b352
marcuwynu23 <marcuwynu23@gmail.com>
2022-09-27 11:57:47 +0800

    Added: Search result logics and whatnots.

--- modules/search/controller/controller.js
+++ modules/search/controller/controller.js
@@ -1,6 +1,11 @@
 const globalConstants = require("../../../constants/constants")
 const { verifyLogin } = require("../../../lib/toolkit")
 
+
+const { getAccountDataBy } = require('../db/db-functions')
+const { getAllClubDataBy } = require("../../clubs/db/db-functions")
+const { getAllEventDataBy } = require("../../events/db/db-functions")
+
 const SEARCH = (req, res) => {
 	verifyLogin(req, res, (accountId, username) => {
 		return res.redirect(globalConstants.ctx.DOMAIN_NAME + "/search/result/?q=" + req.query.q)
@@ -9,13 +14,35 @@ const SEARCH = (req, res) => {
 }
 
 const SEARCH_RESULT = (req, res) => {
+	function query(q, fn) {
+		let filter = { username: q }
+		getAccountDataBy(filter, (err, docs) => {
+			let accounts = docs;
+			filter = { name: q }
+			getAllEventDataBy(filter, (err, docs) => {
+				let events = docs;
+				getAllClubDataBy(filter, (err, docs) => {
+					let clubs = docs;
+					fn(accounts, events, clubs);
+				})
+			})
+		})
+	}
+
+
 	verifyLogin(req, res, (accountId, username) => {
-		console.log(req.query)
-		return res.render('search/search-result.html', {
-			ctx: globalConstants.ctx,
-			accountId: accountId,
-			username: username,
-			query: req.query.q
+		query(req.query.q, (accounts, events, clubs) => {
+			console.log(accounts);
+			console.log(events);
+			console.log(clubs);
+			return res.render('search/search-result.html', {
+				ctx: globalConstants.ctx,
+				accountId: accountId,
+				username: username,
+				accounts: accounts,
+				events: events,
+				clubs: clubs
+			})
 		})
 	})
 
--- modules/search/db/db-functions.js
+++ modules/search/db/db-functions.js
@@ -3,29 +3,16 @@ const db = require('../../../database/datatabase')
 
 function getAccountDataBy(filter, fn) {
 	db.getCollection("accounts", (col) => {
-		col.findOne(filter, (err, docs) => {
+		col.find(filter).toArray((err, docs) => {
 			if (err) console.log(err);
 			fn(err, docs)
 		})
 	})
 }
-
-function getAddressDataBy(filter, fn) {
-	db.getCollection("addresses", (col) => {
-		col.findOne(filter, (err, docs) => {
-			if (err) console.log(err);
-			fn(err, docs)
-		})
-	})
-}
-
-
-
 
 
 
 
 module.exports = {
 	getAccountDataBy: getAccountDataBy,
-	getAddressDataBy: getAddressDataBy
 }
